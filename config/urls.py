3cb8ea86bd19230973b59e85b46f815c5da22919
amirhosein <amirhoseinbu80@gmail.com>
2024-03-23 22:50:50 +0330

    create path for schema

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from schema_graph.views import Schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("schema/",Schema.as_view()),
 ]
